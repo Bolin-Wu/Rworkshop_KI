d05f8c8cb769784c9a465145e46bdfdb8b3c87fa
Bolin Wu <Bolin.Wu@ki.se>
2022-03-10 10:52:06 +0100

    update Merge and reshape data sets

--- example.R
+++ example.R
@@ -64,6 +64,45 @@ View(cov_df)
 #### ----  Merge and reshape data sets -----####
 #----------------------------------------------#
 
+# 4.	Merge datasets “paquid_cog” and “paquid_cov” to a data frame named “paquid”.
 
-paquid = full_join(x = cog_df,y = cov_df,by = c("ID","wave"))
+# with _join function
+paquid = full_join(x = cog_df,y = cov_df,by = c("ID","wave", "age"))
 head(paquid)
+# with merge function
+paquid2 = merge(x = cog_df,y = cov_df)
+head(paquid2)
+# reorder paquid2 so that it has the same column/row order as paquid
+paquid2 = paquid2[order(paquid2$ID),names(paquid)] 
+head(paquid2)
+# great, the results from two functions are the same.
+
+# 5.	Reshape the “paquid” data to wide format.
+
+# first we need to see how many waves are included
+summary.factor(paquid$wave)
+
+head(cog_df)
+head(cov_df)
+# all the variables in cov_df are constan;
+# only variables in cog_df change at different waves
+colnames(cog_df)
+paquid 
+# use spread() function
+spread(data = paquid,value = "MMSE",key = "wave",sep = "MMSE")
+# one short coming of spread function is that it can only spread one column at a time
+# if one wants to spread multiple columns, then one needs to spread tables several times then
+# join the results together
+
+
+# use reshape() function
+# timevar: the variable in long format that differentiates multiple records from the same group or individual.
+# idvar: Columns that will not be affected, stay the same 
+unchange_column = c(names(cov_df),"agedem", "dem", "dem_young")
+wide_paquid = reshape(data = paquid,timevar = "wave", idvar = unchange_column,direction = "wide" )
+head(wide_paquid)
+# it can spread multiple columns at a time
+# but one must be careful when define the "idvar" argument.
+# spread() function is more useful if we only wants to spread one interested variable (e.g. MMSE)
+# reshape() function is mroe useful if we wants to spread multiple columns
+
